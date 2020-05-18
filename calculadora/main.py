071bd47d75a9c2397dea4cf95106a379653ee014
Yuluke <yuluke@hotmail.com>
2020-05-18 08:23:16 +0200

    kata6

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,11 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+import calculator
 
 class mainApp(Tk):
     def __init__(self):
         Tk.__init__ (self)
         self.geometry ("272x300")
         self.title ("Calculadora")
+        self.pack_propagate(False)
+
+        c =calculator.Controlator (self)
+        c.pack(side=TOP, fill=BOTH) 
 
     def start(self):
         self.mainloop()
