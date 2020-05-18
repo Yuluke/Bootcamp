797c621fe4184aae63c38397dfa0a5604d9ab0bd
Yuluke <yuluke@hotmail.com>
2020-05-18 07:19:42 +0200

    kata6

---
+++ calculadora/calculator.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+class Controlador ():
+        pass
+class Display ():
+        pass
+class Selector ():
+        pass
+class CalcButton ():
+        pass
